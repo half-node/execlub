9f6494b6dc99047b5a40faa0e83cbca5138be084
half-node <half-node@yandex.ru>
2024-08-15 20:17:56 +0300

    Порту установлен на стандартный https

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app     = express();
-const port    = 3000;
+const port    = 443;
 
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  res.
+  res.send('Любишь пельмени?');
 });
 
 app.listen(port, () => {
